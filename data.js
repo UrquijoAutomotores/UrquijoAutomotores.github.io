bfc9c2e289c189dd672b03dc13e348ba74b06be7
UrquijoAutomotores <urquijoautomotores@gmail.com>
2026-05-19 15:45:23 -0300

    Update data.js

--- data.js
+++ data.js
@@ -21,6 +21,27 @@ const inventoryData = [
         features: ['Sistema de conectividad SYNC con control por voz', 'Llantas de aleación de 15"', 'Doble airbag frontal y frenos ABS', 'Aire acondicionado', 'Alzacristales y espejos eléctricos']
     },
     {
+        id: 13,
+        brand: 'Ford',
+        model: 'Fiesta Kinetic',
+        trim: 'Titanium AT',
+        price: 'Consultar',
+        year: '2015',
+        km: '129.000KM',
+        fuel: 'Nafta',
+        condition: 'Usados',
+        image: './Autos Pagina/Fiesta/PortadaFiesta.webp',
+        gallery: [
+            './Autos Pagina/Fiesta/Fiesta 2.webp',
+            './Autos Pagina/Fiesta/Fiesta 3.webp',
+            './Autos Pagina/Fiesta/Fiesta 4.webp'
+        ],
+        status: 'Disponible',
+        available: true,
+        description: 'El Ford Fiesta Kinetic S Plus 2014 destaca por su atractivo y moderno diseño global. Equipado con el confiable motor 1.6L Sigma de 120 CV, ofrece un excelente equilibrio entre eficiencia y respuesta urbana, sumado a un completo nivel de equipamiento en confort y seguridad para su segmento.',
+        features: ['Sistema de conectividad SYNC con control por voz', 'Llantas de aleación de 15"', 'Doble airbag frontal y frenos ABS', 'Aire acondicionado', 'Alzacristales y espejos eléctricos']
+    },
+    {
         id: 2,
         brand: 'Chevrolet',
         model: 'Onix',
