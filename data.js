4eafbf4aeda77a924b389aa58c80e9e90a6f28e9
UrquijoAutomotores <urquijoautomotores@gmail.com>
2026-07-20 08:47:45 -0300

    Actualizar data.js

--- data.js
+++ data.js
@@ -67,29 +67,6 @@ const inventoryData = [
         description: 'El Ford Fiesta Kinetic Titanium es la versión tope de gama. Destaca por su atractivo diseño, comodidad de la caja automática, excelente nivel de seguridad con 7 airbags, techo solar eléctrico y tecnología de conectividad avanzada.',
         features: ['Caja Automática', 'Techo solar eléctrico', 'Climatizador automático', '7 Airbags', 'Control de velocidad crucero']
     },
-    {
-        id: 14,
-        brand: 'Chevrolet',
-        model: 'Astra',
-        trim: 'GL',
-        price: 'Consultar',
-        year: '2008',
-        km: '126.000KM',
-        fuel: 'Nafta',
-        condition: 'Usados',
-        image: './Autos Pagina/Astra-Gl-2008/astra-costadoderecho',
-        gallery: [
-            './Autos Pagina/Astra-Gl-2008/astra-costado.webp',
-            './Autos Pagina/Astra-Gl-2008/astra-costadoderecho.webp',
-            './Autos Pagina/Astra-Gl-2008/ParteTrasera-astra.webp',
-            './Autos Pagina/Astra-Gl-2008/astra-interior.webp'
-        ],
-        status: 'Reservado',
-        available: false,
-        description: 'Chevrolet Astra GL modelo 2008 sigue siendo uno de los referentes más buscados en el mercado de usados, ideal para quienes priorizan el confort de marcha y la confiabilidad a largo plazo. Equipado con el reconocido motor naftero 2.0 de 8 válvulas, ofrece un equilibrio perfecto entre agilidad urbana y un andar sereno en ruta, respaldado por una mecánica noble, repuestos accesibles y un mantenimiento muy sencillo.',
-        features: ['Levantavidrios eléctricos delanteros y traseros con sistema "One Touch".', 'Aire acondicionado de gran caudal y dirección asistida', 'Llantas de aleación originales de 15 pulgadas', 'Apertura y cierre de puertas a distancia con comando en llave']
-    },
-    {
         id: 3,
         brand: 'Fiat',
         model: 'Palio',
