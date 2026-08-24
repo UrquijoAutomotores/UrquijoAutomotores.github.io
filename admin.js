7f58cc99b90adca515fbfc54bf18372baaf87185
UrquijoAutomotores <urquijoautomotores@gmail.com>
2026-08-24 17:22:53 -0300

    Add files via upload

--- admin.js
+++ admin.js
@@ -56,7 +56,7 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 
     // FunciÃ³n para mostrar mensajes de estado
-    function showMsg(text, type) {
+    window.showMsg = function(text, type) {
         statusMsg.innerText = text;
         statusMsg.classList.remove('hidden', 'bg-blue-100', 'text-blue-700', 'bg-green-100', 'text-green-700', 'bg-red-100', 'text-red-700');
         
